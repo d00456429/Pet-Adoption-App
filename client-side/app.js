3071d8fbf99ceb66e70adffd09db8bd6a3607f22
Saul Aguiar <d00456429@CMP-SCC112-1107.local>
2024-06-26 10:22:18 -0600

    make client-side directory

---
+++ client-side/app.js
@@ -0,0 +1,11 @@
+const URL = "http://localhost:8080";
+
+Vue.createApp({
+  data() {
+    return {};
+  },
+  methods: {},
+  created: function () {
+    console.log("Created");
+  },
+}).mount("#app");
