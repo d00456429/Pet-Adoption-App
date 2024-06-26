93787dec0e79a28a00516c757447317abacfa7d8
Alec Searle <d00424288@CMP-SCC112-1108.local>
2024-06-26 10:36:48 -0600

    app.js layout

--- client-side/app.js
+++ client-side/app.js
@@ -1,11 +1,38 @@
 const URL = "http://localhost:8080";
 
 Vue.createApp({
-  data() {
-    return {};
-  },
-  methods: {},
-  created: function () {
-    console.log("Created");
-  },
+	data() {
+		return {
+			page: 1,
+			applications: [],
+			pets: [],
+			petId: {
+				name: "",
+				species: "",
+				breed: "",
+				age: "",
+				gender: "",
+			},
+			newApplication: {
+				name: "",
+				phoneNum: "",
+				email: "",
+				petId: "",
+			},
+		};
+	},
+	methods: {
+		setPage: function (page) {
+			this.page = page;
+		},
+		getListings: function () {},
+		createListing: function () {},
+		deleteListing: function (listingId) {},
+		getApplications: function () {},
+		createApplication: function () {},
+		changePage: function (page) {},
+	},
+	created: function () {
+		console.log("Created");
+	},
 }).mount("#app");
