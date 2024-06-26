46f8cbfa17e66190ba265443cc0a06bae324ae70
Saul Aguiar <d00456429@CMP-SCC112-1107.local>
2024-06-26 11:05:04 -0600

    petlisting

--- client-side/app.js
+++ client-side/app.js
@@ -1,38 +1,53 @@
 const URL = "http://localhost:8080";
 
 Vue.createApp({
-	data() {
-		return {
-			page: 1,
-			applications: [],
-			pets: [],
-			petId: {
-				name: "",
-				species: "",
-				breed: "",
-				age: "",
-				gender: "",
-			},
-			newApplication: {
-				name: "",
-				phoneNum: "",
-				email: "",
-				petId: "",
-			},
-		};
-	},
-	methods: {
-		setPage: function (page) {
-			this.page = page;
-		},
-		getListings: function () {},
-		createListing: function () {},
-		deleteListing: function (listingId) {},
-		getApplications: function () {},
-		createApplication: function () {},
-		changePage: function (page) {},
-	},
-	created: function () {
-		console.log("Created");
-	},
+  data() {
+    return {
+      page: 1,
+      applications: [],
+      pets: [
+        {
+          name: "Arma",
+          species: "Armadillo",
+          breed: "Armadillo",
+          age: "27",
+          gender: "Male",
+        },
+        {
+          name: "Bandit",
+          species: "Gecko",
+          breed: "Leopard-tail",
+          age: "1",
+          gender: "Male",
+        },
+      ],
+      newPet: {
+        name: "",
+        species: "",
+        breed: "",
+        age: "",
+        gender: "",
+      },
+      newApplication: {
+        name: "",
+        phoneNum: "",
+        email: "",
+        petId: "",
+      },
+    };
+  },
+  methods: {
+    setPage: function (page) {
+      this.page = page;
+    },
+    getListings: function () {},
+    createListing: function () {},
+    deleteListing: function (listingId) {},
+    getApplications: function () {},
+    createApplication: function () {},
+    changePage: function (page) {},
+  },
+  created: function () {
+    console.log("Created");
+  },
 }).mount("#app");
